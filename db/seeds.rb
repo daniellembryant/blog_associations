1fda06f1a48d58868f4dc15b8f53f159d55a80d7
daniellembryant <danbry08@yahoo.com>
2021-09-16 16:47:12 -0500

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# Post.create!([
+#   {title: "Positive Thinking", body: "Learn how to think positively", author: "B.Baker"},
+#   {title: "Making Friends After 30", body: "A guide on how to make friends later in life", author: "KK. Jackson"},
+#   {title: "Are Dogs Really Man's Best Friend?", body: "An article that explores if dogs are really man's best friend", author: "Jay Smith"}
+# ])
+
+# Tag.create!([
+#   {name: "Positivity"},
+#   {name: "Random"},
+#   {name: "How To"}
+# ])
+
+# PostTag.create!([
+#   {post_id: 1 , tag_id: 1},
+#   {post_id: 1, tag_id: 2 },
+#   {post_id: 2, tag_id: 3 },
+#   {post_id: 3, tag_id: 3}
+# ])
